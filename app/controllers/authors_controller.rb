d6b39a3f6ab454fbb031571f0952dafdaa3a4b29
Soundararajan <soundararajan@usa.net>
2015-12-27 21:23:21 -0800

    Implement auuthors controller show action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,9 @@ class AuthorsController < ApplicationController
   def index
 
   end
+
+  def show
+    #require 'pry'; binding.pry
+    @author = Author.find(params[:id])
+  end
 end
--- spec/controllers/author_controller_spec.rb
+++ spec/controllers/author_controller_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe AuthorsController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "Get #show" do 
+    it "returns a successful http status code" do
+      author = Fabricate(:author)
+
+      get :show, id: author.id
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
