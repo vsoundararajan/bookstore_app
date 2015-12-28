1251df9142f09bafa2db86b9d778a6f3eb866ce9
Soundararajan <soundararajan@usa.net>
2015-12-27 21:13:47 -0800

    Implement auuthors controller index action

---
+++ spec/controllers/author_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe AuthorsController, :type => :controller do
+  describe "GET #index" do
+    it "returns a successful http request status code" do
+      get :index
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
