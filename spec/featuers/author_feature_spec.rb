417f6205d10eb5efab47ad0c7d09e738907fa6dd
Soundararajan <soundararajan@usa.net>
2015-12-31 14:51:23 -0800

    Create unc=successful author feature

--- spec/featuers/author_feature_spec.rb
+++ spec/featuers/author_feature_spec.rb
@@ -15,4 +15,18 @@ RSpec.feature "Creating Authors" do
 
     expect(page).to have_content("Author has been created")
   end
+
+  scenario "with invalid inputs fails" do
+    visit root_path
+
+    click_link "Authors"
+    click_link "Add New author"
+
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+
+    click_button "Create Author"
+
+    expect(page).to have_content("Author has not been created")
+  end
 end
