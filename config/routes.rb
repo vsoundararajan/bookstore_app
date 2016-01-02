33a55bd57d72156c34dde1f4790e3e8b9aad5960
Soundararajan <soundararajan@usa.net>
2016-01-01 23:59:43 -0800

    Implement new and create session action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: "catalogs#index"
 
+
+  get "/signin", to: 'sessions#new', as: 'signin'
+
+  post "/signin", to: 'sessions#create'
+
   resources :authors
   resources :publishers
   resources :books
