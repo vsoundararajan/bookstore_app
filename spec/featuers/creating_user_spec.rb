ca9ed23fb20d9fdf14b227cdd42129e82f3b961b
Soundararajan <soundararajan@usa.net>
2016-01-01 18:42:25 -0800

    Implement user sign up

---
+++ spec/featuers/creating_user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Creating users" do
+  scenario "with valid credentials" do
+    visit root_path
+
+    click_link "Sign up"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "password"
+    fill_in "user_password_confirmation", with: "password"
+
+    click_button "Create User"
+
+    expect(page).to have_content('User has been created')
+  end
+
+  scenario "with invalid credentials" do
+    visit root_path
+
+    click_link "Sign up"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "john@example.com"
+    fill_in "Password", with: "password"
+    fill_in "user_password_confirmation", with: "password"
+
+    click_button "Create User"
+
+    expect(page).to have_content('User has not been created')
+  end
+end
