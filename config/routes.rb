ebedded907521e2dd4d23ee7158fd76aa129f438
Soundararajan <soundararajan@usa.net>
2016-01-02 16:11:19 -0800

    Master ommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: "catalogs#index"
 
+
+  get "/signup", to: 'users#new', as: 'signup'
+  get "/signin", to: 'sessions#new', as: 'signin'
+
+  post "/signin", to: 'sessions#create'
+
   resources :authors
   resources :publishers
   resources :books
