e593687741e4b42bd93ac7270a3bfbf42f2e7058
Soundararajan <soundararajan@usa.net>
2016-01-03 23:17:10 -0800

    Implement line total and total sale methods

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe CartItem, type: :model do
   it { should belong_to(:cart) }
   it { should belong_to(:book)} 
+
+  describe "#line_total" do
+    it "should return the line total cost" do
+      book = Fabricate(:book, price: 15)
+      cart = Fabricate(:cart)
+      cart_item = Fabricate(:cart_item, cart_id: cart.id, book_id: book.id, 
+        price: book.price, quantity: 2)
+
+      expect(cart_item.line_total).to eq(30)
+    end
+  end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,4 +3,20 @@ require 'rails_helper'
 RSpec.describe Cart, type: :model do
   it { should have_many(:cart_items) }
   it { should have_many(:books).through(:cart_items) }
+
+  describe "#total_sale" do
+   it "should return the total sale amount for the cart" do
+     book1 = Fabricate(:book, price: 10)
+     book2 = Fabricate(:book, price: 15)
+
+     cart = Fabricate(:cart)
+     cart_item1 = Fabricate(:cart_item, cart_id: cart.id, book_id: book1.id, 
+        price: book1.price, quantity: 1)
+     cart_item2 = Fabricate(:cart_item, cart_id: cart.id, book_id: book2.id, 
+        price: book2.price, quantity: 2)
+     cart.cart_items << cart_item1
+     cart.cart_items << cart_item2
+     expect(cart.total_sale).to eq(40)
+   end
+  end
 end
